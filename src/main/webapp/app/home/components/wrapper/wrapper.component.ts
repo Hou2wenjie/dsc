2df6fd6bf5d68f965159f76150192d4ed05a5c8d
Wenjie Hou <wenjie.hou@tum.de>
2020-04-27 12:11:57 +0200

    add buggy home page

---
+++ src/main/webapp/app/home/components/wrapper/wrapper.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-wrapper',
+  templateUrl: './wrapper.component.html',
+  styleUrls: ['../../home.scss']
+})
+export class WrapperComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
